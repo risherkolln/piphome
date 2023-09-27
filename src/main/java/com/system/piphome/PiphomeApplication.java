e22ba69c3fafbed128ecaedaf5c7e29c400e813e
risherkolln <105298089+risherkolln@users.noreply.github.com>
2023-09-27 20:37:42 -0300

    first commit
    add the maven base project created from initializr

---
+++ src/main/java/com/system/piphome/PiphomeApplication.java
@@ -0,0 +1,13 @@
+package com.system.piphome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiphomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiphomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/piphome/PiphomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.piphome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiphomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
